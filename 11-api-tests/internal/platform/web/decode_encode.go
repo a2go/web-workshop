0dc6095a81469f6e00bc07085db61069ed5f22cd
Nick Stogner <nicholas.stogner@gmail.com>
2018-11-08 21:18:09 -0500

    Add section 11 - API Tests

---
+++ 11-api-tests/internal/platform/web/decode_encode.go
@@ -0,0 +1,38 @@
+package web
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/ardanlabs/service-training/11-api-tests/internal/platform/log"
+)
+
+type HandlerFunc func(r *http.Request) (interface{}, error)
+
+func Decode(r *http.Request, x interface{}) error {
+	if err := json.NewDecoder(r.Body).Decode(x); err != nil {
+		return ErrorWithStatus(err, http.StatusUnprocessableEntity)
+	}
+
+	return nil
+}
+
+func Encode(f HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		res, err := f(r)
+		if err != nil {
+			log.Log("request resulted in error", "error", err)
+
+			serr := toStatusError(err)
+			w.WriteHeader(serr.status)
+
+			res = struct {
+				Error string `json:"error"`
+			}{serr.ExternalError()}
+		}
+
+		if err := json.NewEncoder(w).Encode(res); err != nil {
+			log.Log("responding with json", "error", err)
+		}
+	}
+}
---
+++ 11-api-tests/tests/salesapi/api_test.go
@@ -0,0 +1,91 @@
+package api_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/ardanlabs/service-training/11-api-tests/internal/products"
+	"github.com/kelseyhightower/envconfig"
+)
+
+func TestAPI(t *testing.T) {
+	var cfg struct {
+		HTTP struct {
+			Address string `default:"localhost:8000"`
+		} `envconfig:"http"`
+	}
+	envconfig.MustProcess("test", &cfg)
+
+	c := http.Client{
+		Timeout: 15 * time.Second,
+	}
+
+	// Create a product.
+	var p0created products.Product
+	t.Run("create", func(t *testing.T) {
+		resp, err := c.Post(fmt.Sprintf("http://%s/v1/products", cfg.HTTP.Address), "application/json", strings.NewReader(`{
+			"name": "product0",
+			"cost": 55,
+			"quantity": 6
+		}`))
+		if err != nil {
+			t.Fatalf("posting: %s", err)
+		}
+		if exp, got := http.StatusOK, resp.StatusCode; exp != got {
+			t.Fatalf("posting: expected status code %v, got %v", exp, got)
+		}
+		defer resp.Body.Close()
+
+		if err := json.NewDecoder(resp.Body).Decode(&p0created); err != nil {
+			t.Fatalf("decoding: %s", err)
+		}
+		if p0created.ID == "" {
+			t.Fatal("expected non-empty product id")
+		}
+		// TODO: More product field assertions.
+	})
+
+	// Get the created product.
+	var p0gotten products.Product
+	t.Run("get", func(t *testing.T) {
+		resp, err := c.Get(fmt.Sprintf("http://%s/v1/products/%s", cfg.HTTP.Address, p0created.ID))
+		if err != nil {
+			t.Fatalf("getting: %s", err)
+		}
+		if exp, got := http.StatusOK, resp.StatusCode; exp != got {
+			t.Fatalf("getting: expected status code %v, got %v", exp, got)
+		}
+		defer resp.Body.Close()
+
+		if err := json.NewDecoder(resp.Body).Decode(&p0gotten); err != nil {
+			t.Fatalf("decoding: %s", err)
+		}
+		if exp, got := p0created, p0gotten; exp != got {
+			t.Fatalf("expected %+v, got %+v", exp, got)
+		}
+	})
+
+	// List products.
+	t.Run("list", func(t *testing.T) {
+		resp, err := c.Get(fmt.Sprintf("http://%s/v1/products", cfg.HTTP.Address))
+		if err != nil {
+			t.Fatalf("getting: %s", err)
+		}
+		if exp, got := http.StatusOK, resp.StatusCode; exp != got {
+			t.Fatalf("getting: expected status code %v, got %v", exp, got)
+		}
+		defer resp.Body.Close()
+
+		var list []products.Product
+		if err := json.NewDecoder(resp.Body).Decode(&list); err != nil {
+			t.Fatalf("decoding: %s", err)
+		}
+		if len(list) == 0 {
+			t.Fatal("expected non-empty product list")
+		}
+	})
+}
