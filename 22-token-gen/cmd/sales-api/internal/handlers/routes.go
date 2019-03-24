bd74f162bda3f13d6c4970ebbd749b1000456455
Jacob Walker <jacob@ardanlabs.com>
2019-03-24 00:04:47 -0400

    Add section for generating JWTs

---
+++ 22-token-gen/cmd/sales-api/internal/handlers/routes.go
@@ -0,0 +1,43 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/jmoiron/sqlx"
+
+	"github.com/ardanlabs/garagesale/internal/mid"
+	"github.com/ardanlabs/garagesale/internal/platform/auth"
+	"github.com/ardanlabs/garagesale/internal/platform/web"
+)
+
+// API constructs an http.Handler with all application routes defined.
+func API(db *sqlx.DB, errorLog, infoLog *log.Logger, authenticator *auth.Authenticator) http.Handler {
+
+	app := web.New(errorLog, mid.RequestLogger(infoLog), web.ErrorHandler(errorLog), mid.Metrics)
+
+	{
+		c := Checks{db: db}
+		app.Handle(http.MethodGet, "/v1/health", c.Health)
+	}
+
+	{
+		p := Products{db: db, log: infoLog}
+
+		app.Handle(http.MethodPost, "/v1/products", p.Create)
+		app.Handle(http.MethodGet, "/v1/products", p.List)
+		app.Handle(http.MethodGet, "/v1/products/{id}", p.Get)
+		app.Handle(http.MethodPut, "/v1/products/{id}", p.Update)
+		app.Handle(http.MethodDelete, "/v1/products/{id}", p.Delete)
+
+		app.Handle(http.MethodPost, "/v1/products/{id}/sales", p.AddSale)
+		app.Handle(http.MethodGet, "/v1/products/{id}/sales", p.ListSales)
+	}
+
+	{
+		u := Users{db: db, authenticator: authenticator}
+		app.Handle(http.MethodGet, "/v1/users/token", u.Token)
+	}
+
+	return app
+}
