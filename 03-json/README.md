60df94dc52f0d09437d4c8ff72ed4abdc196c1e0
Jacob Walker <jacob@ardanlabs.com>
2019-03-25 15:26:56 -0400

    Add JSON section

---
+++ 03-json/README.md
@@ -0,0 +1,9 @@
+# 3. JSON
+
+- Create a type `Product` with fields for Name, Cost, and Quantity
+- Rename the `Echo` handler to `ListProducts`
+- Create a slice of Product values with some dummy data.
+- Marshal the slice to JSON and write it to the client.
+- Use `w.WriteHeader` to explicitly set the response status code.
+- Include the Content-Type header so clients understand the response.
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
---
+++ 03-json/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	server := http.Server{
+		Addr:         ":8000",
+		Handler:      http.HandlerFunc(ListProducts),
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+	}
+
+	serverErrors := make(chan error, 1)
+	go func() {
+		log.Println("server listening on", server.Addr)
+		serverErrors <- server.ListenAndServe()
+	}()
+
+	osSignals := make(chan os.Signal, 1)
+	signal.Notify(osSignals, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case err := <-serverErrors:
+		log.Fatalf("error: listening and serving: %s", err)
+
+	case <-osSignals:
+		log.Println("caught signal, shutting down")
+
+		// Give outstanding requests 5 seconds to complete.
+		const timeout = 5 * time.Second
+		ctx, cancel := context.WithTimeout(context.Background(), timeout)
+		defer cancel()
+
+		if err := server.Shutdown(ctx); err != nil {
+			log.Printf("error: gracefully shutting down server: %s", err)
+			if err := server.Close(); err != nil {
+				log.Printf("error: closing server: %s", err)
+			}
+		}
+	}
+
+	log.Println("done")
+}
+
+// Product is an item we sell.
+type Product struct {
+	Name     string `json:"name"`
+	Cost     int    `json:"cost"`
+	Quantity int    `json:"quantity"`
+}
+
+// ListProducts is an HTTP Handler for returning a list of Products.
+func ListProducts(w http.ResponseWriter, r *http.Request) {
+	products := []Product{
+		{Name: "Comic Books", Cost: 50, Quantity: 42},
+		{Name: "McDonalds Toys", Cost: 75, Quantity: 120},
+	}
+
+	data, err := json.Marshal(products)
+	if err != nil {
+		log.Println("error marshalling result", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	if _, err := w.Write(data); err != nil {
+		log.Printf("error: encoding response: %s", err)
+	}
+}
