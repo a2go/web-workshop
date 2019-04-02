669874f18ed0b48949ba485ee6284363b7c342ed
Jacob Walker <jacob@ardanlabs.com>
2019-04-02 12:00:00 -0500

    Remove blank lines around PasswordHash

--- 27-useradd/internal/users/users.go
+++ 27-useradd/internal/users/users.go
@@ -13,15 +13,13 @@ import (
 
 // User represents someone with access to our system.
 type User struct {
-	ID    string   `db:"user_id" json:"id"`
-	Name  string   `db:"name" json:"name"`
-	Email string   `db:"email" json:"email"`
-	Roles []string `db:"roles" json:"roles"`
-
-	PasswordHash []byte `db:"password_hash" json:"-"`
-
-	DateCreated time.Time `db:"date_created" json:"date_created"`
-	DateUpdated time.Time `db:"date_updated" json:"date_updated"`
+	ID           string    `db:"user_id" json:"id"`
+	Name         string    `db:"name" json:"name"`
+	Email        string    `db:"email" json:"email"`
+	Roles        []string  `db:"roles" json:"roles"`
+	PasswordHash []byte    `db:"password_hash" json:"-"`
+	DateCreated  time.Time `db:"date_created" json:"date_created"`
+	DateUpdated  time.Time `db:"date_updated" json:"date_updated"`
 }
 
 // NewUser contains information needed to create a new User.
